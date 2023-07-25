6927f54a152c7e06f83200caa8a5d0ae451d27f1
CBRA-Melanie <melanie@cbra-media.com>
2023-07-25 11:48:11 +0200

    clean up

--- cbra-ga-opt-out.php
+++ cbra-ga-opt-out.php
@@ -2,37 +2,27 @@
 /**
  * Plugin Name:     CBRA Google Analytics Opt Out
  * Plugin URI:      https://cbra.digital
- * Description:     Sets a cookie that tells Google not to track. 
+ * Description:     Sets a cookie that tells Google not to track. Use Shortcode [cb_ga_opt_out property="G-..."]
  * Author:          CBRA Digital
  */
 
 add_shortcode('cb_ga_opt_out', 'cb_shortcode_opt_out');
-function cb_shortcode_opt_out() {
+function cb_shortcode_opt_out($atts) {
+    $atts = shortcode_atts(array(
+		'property' => 'default'
+	), $atts, 'cb_ga_opt_out' );
+
     ob_start();
     ?>
     <a href="javascript:gaOptout();" onclick="alert('Google Analytics wurde deaktiviert');">Erfassung von Daten durch Google Analytics für diese Website deaktivieren</a>
     <?php
+    wp_register_script('ga-opt-out', WP_PLUGIN_URL . '/cbra-ga-opt-out/cbra-ga-opt-out.js');
+    wp_localize_script('ga-opt-out', 'data', array(
+        'property'  =>  $atts['property'],
+    ));
+    wp_enqueue_script('ga-opt-out');
+
     return ob_get_clean();
 };
 
-function cb_do_opt_out() {
-    ob_start();
-    ?>
-   <script>
-            var gaProperty = 'G-XXXXXX';
-            var disableStr = 'ga-disable-' + gaProperty;
-
-            if (document.cookie.indexOf(disableStr + '=true') > -1) {
-                window[disableStr] = true;
-            }
-            
-            function gaOptout() {
-                document.cookie = disableStr + '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/';
-                window[disableStr] = true;
-            }
-    </script>
-    <?php
-    echo ob_get_clean();
-}
-add_action('wp_head', 'cb_do_opt_out');
  ?>
