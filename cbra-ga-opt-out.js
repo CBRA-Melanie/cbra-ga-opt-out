621abaf152b562e1123e2fad89ca7f95383e3813
CBRA-Melanie <melanie@cbra-media.com>
2023-07-25 13:09:25 +0200

    styles

--- cbra-ga-opt-out.js
+++ cbra-ga-opt-out.js
@@ -9,3 +9,11 @@ function gaOptout() {
     document.cookie = disableStr + '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/';
     window[disableStr] = true;
 }
+
+const link = document.getElementById('cb-ga-opt-out');
+
+link.addEventListener("click", (e) => {
+    e.preventDefault();
+    alert(data.alert);
+    gaOptout();
+});
--- cbra-ga-opt-out.php
+++ cbra-ga-opt-out.php
@@ -9,16 +9,20 @@
 add_shortcode('cb_ga_opt_out', 'cb_shortcode_opt_out');
 function cb_shortcode_opt_out($atts) {
     $atts = shortcode_atts(array(
-		'property' => 'default'
+		'property'      => 'default',
+        'text'          => 'Erfassung von Daten durch Google Analytics für diese Website deaktivieren',
+        'alert'         => 'Google Analytics wurde deaktiviert',
+        'color'         => 'black'
 	), $atts, 'cb_ga_opt_out' );
 
     ob_start();
     ?>
-    <a href="javascript:gaOptout();" onclick="alert('Google Analytics wurde deaktiviert');">Erfassung von Daten durch Google Analytics für diese Website deaktivieren</a>
+    <a href="#" id="cb-ga-opt-out" style="color: <?= $atts['color'] ?>; font-weight: bold; text-decoration: none"><?= $atts['text'] ?></a>
     <?php
     wp_register_script('ga-opt-out', WP_PLUGIN_URL . '/cbra-ga-opt-out/cbra-ga-opt-out.js');
     wp_localize_script('ga-opt-out', 'data', array(
         'property'  =>  $atts['property'],
+        'alert'     => $atts['alert']
     ));
     wp_enqueue_script('ga-opt-out');
 
